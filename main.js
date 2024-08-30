29be03e2653bbdb47d768035f9948caee1310b75
Am1d1v <dimadiman1adima@gmail.com>
2024-08-30 13:33:12 +0300

    Electron create main window

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Msg');
+const {app, BrowserWindow} = require('electron');
+
+function createMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: 'ImageShrinker',
+        width: 600,
+        height: 600
+    })
+};
+
+app.on('ready', createMainWindow);
