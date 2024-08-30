f4acde3a89c323adad87af9f2eb517bc480fc7e5
Am1d1v <dimadiman1adima@gmail.com>
2024-08-30 15:15:33 +0300

    Assets Added. Window file path added

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const {app, BrowserWindow} = require('electron');
 
+let mainWindow
+
 function createMainWindow(){
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         title: 'ImageShrinker',
         width: 600,
-        height: 600
-    })
+        height: 600,
+        icon: './assets/Icon_256x256.png'
+    });
+
+    // File Path
+    //mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+    mainWindow.loadFile('./app/index.html');
 };
+
 
 app.on('ready', createMainWindow);
