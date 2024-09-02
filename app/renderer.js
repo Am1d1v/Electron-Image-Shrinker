d537465bcc0289e3656e29642e92826df45cd555
Am1d1v <dimadiman1adima@gmail.com>
2024-09-02 14:07:57 +0300

    IPC Communication

--- app/renderer.js
+++ app/renderer.js
@@ -2,4 +2,19 @@
 const path = require('path');
 const os = require('os');
 
-document.querySelector('#output-path').innerText = path.join(__dirname)
+document.querySelector('#output-path').innerText = path.join(__dirname);
+
+
+const form = document.querySelector('#image-form');
+const slider = document.querySelector('#slider');
+const img = document.querySelector('#img');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const imgPath = img.files[0];
+    const quality = slider.value;
+
+    console.log(imgPath, quality);
+});
+
